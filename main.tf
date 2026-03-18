73fdbcbaafb8c4114aa11eded26d93101b1f23f7
noa-se <noa.hernandez-piette@ecole-se.org>
2026-03-18 11:10:22 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "noa_final_eval_rg"
+  location = "France Central"
+}
+
+
+resource "azurerm_storage_account" "static_site" {
+  name                     = "noafinalevalsa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "error.html"
+  }
+}
+
+
